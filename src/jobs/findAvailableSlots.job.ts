4b12e3658e6156bfde924622127fd5aac5c9032e
katz2222 <xd.akatz17@idf.il>
2025-08-28 10:11:02 +0300

    set job to run headless

--- src/jobs/findAvailableSlots.job.ts
+++ src/jobs/findAvailableSlots.job.ts
@@ -4,9 +4,13 @@ import { findAvailableSlots } from "@services/findAvailableSlots.service";
 import { AUTH_STATE_FILE_PATH } from "env-variables";
 
 async function runJob() {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext({
     storageState: AUTH_STATE_FILE_PATH,
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+    viewport: { width: 1366, height: 768 },
+    locale: "en-US",
   });
   const page = await context.newPage();
 
